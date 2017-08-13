2b7d6fe6e7aad58fd4aec03665527c22f0e9a26b
xlcocoon <xlcocoon@foxmail.com>
2017-08-13 23:49:17 +0800

    V2
    V2

--- src/com/xialei/git/test/Client.java
+++ src/com/xialei/git/test/Client.java
@@ -9,6 +9,7 @@ public class Client {
 		System.out.println("hello 0407 3");
 		System.out.println("hello p2");
 		System.out.println("回复 Github服务器");
+		System.out.println("回复 Github服务器V2");
 	}
 
 }
