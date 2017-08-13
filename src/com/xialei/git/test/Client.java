f223b9e238cd7eeb1a9bb7655bc928a45f20d6a8
xlco <xlcocoon@foxmail.com>
2017-08-13 23:28:32 +0800

    V2

--- src/com/xialei/git/test/Client.java
+++ src/com/xialei/git/test/Client.java
@@ -2,8 +2,10 @@ package com.xialei.git.test;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("hello 0407");
+		System.out.println("hello 0407 2");
 
 	}
 
